bb12eea5ac308d676079f5d655cecba5f03cbbda
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2015-12-05 21:50:44 +0100

    Changed: Ability csv -> MongoDB
    I have finally managed to get it working to write the csv content into the mongoDB.
    At the moment this only works for our sample dataset.
    I will refactor the code and improve it a little bit and upload it again the next hours.

--- _Projekt/src/mongodb.py
+++ _Projekt/src/mongodb.py
@@ -1,21 +1,59 @@
 # TODO: folding comments : select Text and press CTRL + ALT + T
-# TODO: implement if exist method to not always generate new .csv-files
 # TODO: automatic writing into the database needs to be done then
+# TODO: Sort database
 
 from pymongo import MongoClient                         # necessary to interact with MongoDB
 import pandas as pd                                     # necessary for fast .csv-processing
 
+
+csv_Loaded_CSV = ""
 csv_Starting_Counter = 2001                                     # The starting number correspondig to our first data set        (Crimes_-_2001.csv)
 csv_Ending_Counter = 2015                                       # The ending number corresponding to our last data set          (Crimes_-_2014.csv)
-csv_Columns_To_Keep = ['Date','Block']                          # Contains all columns which are to be kept within newly generated csv data files
+csv_Columns_To_Keep = ['Date','IUCR','Primary Type','Description','Location Description','Arrest','Domestic','District','FBI Code','Year','Latitude','Longitude']                          # Contains all columns which are to be kept within newly generated csv data files - necessary for process_CSV_Data
+csv_Actual_Row = ""
+
 
 client = MongoClient                                            # will be modified by connect_To_MongoDB()
 mongo_DB_Chicago = MongoClient                                  # will be modified by get_DB_Instance()
-mongo_dataSet_Row = {}                                          # will be modified by generate_DataSet_To_Be_Written_To_DB()
+mongo_DataSet_Row = {}                                          # will be modified by generate_DataSet_To_Be_Written_To_DB()
 
 
 # <editor-fold desc="CSV data operations happen inside here">
-print "Sackl picka!"
+# <editor-fold desc="Reads CSV data">
+# Source: http://pandas.pydata.org/pandas-docs/stable/api.html
+def read_CSV_Data():
+	global csv_Loaded_CSV
+
+	csv_Loaded_CSV = pd.read_csv('..\datasets\_sample_2014.csv')
+
+	#for i in range(starting_Counter, ending_Counter):                                       # Iterates over all .CSV file
+#        print str(i) + ".er Durchgang"
+#        df = pd.read_csv('..\datasets\Crimes_-_' + str(i) +'.csv')
+#        df_new = df[columns_To_keep]
+#        df_new.to_csv('..\datasets\_MODIFIED_Crimes_-_' + str(i) +'.csv', index=False)        # Index=False ignores creation of a counting column
+
+
+
+
+# </editor-fold>
+
+def process_CSV_Data():
+	global csv_Loaded_CSV
+	global csv_Columns_To_Keep
+	global csv_Actual_Row
+	global mongo_DataSet_Row                                       # refers to the global variable
+
+
+	csv_Processed = csv_Loaded_CSV[csv_Columns_To_Keep]            # Reduce the read csv data to only contain defined columns
+	csv_Processed = csv_Processed.T.to_dict('list')                # Converts the newly defined csv-data to a list
+
+	for key in sorted(csv_Processed):
+		csv_Actual_Row = csv_Processed[key]                          # Contains the actual iterated row
+		create_Dictionary_For_DB_Write()                            # Creates a "dictionary" out of the given file data
+		write_Data_To_DB()                                          # Writes that actuallyprocessed / iterated row into the MongoDB
+
+
+
 # </editor-fold>
 
 # <editor-fold desc="MongoDB operations happen inside here">
@@ -51,57 +89,55 @@ def get_DB_Instance():
 
         mongo_DB_Chicago = client.Chicago_Crime_Database        # refers to the crime db and creates it if non existent
 
-# <editor-fold desc="Generates data rows which will be written in to the referenced data base (Chicago_Crime_Database">
-# Generates a Dataset which will be Written into the, previously referenced, data base instace
-# Which is here Chicago_Crime_Data_Base
-# TODO: Depending on tables the tablename (i.E. chicago_2014) needs to change!
-# </editor-fold>
-def generate_DataSet_To_Be_Written_To_DB():
-        global mongo_dataSet_Row                                # refers to the global variable
 
-        mongo_dataSet_Row = {
-		"author": "Test_DB",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],   # Defines an array within a column
-        "date": "Bin eine 111 Rasdswwadsaow",
-		}
+# <editor-fold desc="Creates directory information for MongoDB">
+# This method processes the actual row, to be iterated at the moment
+# And builds a "directory" object out of the single informations of the row
+# </editor-fold>
+def create_Dictionary_For_DB_Write():
+		global mongo_DataSet_Row                                # refers to the global variable
+		global csv_Actual_Row                                    # refers to the global variable
+
+		# Builds a "dictionary" out of the information which the actually iterated row contains
+		mongo_DataSet_Row = dict({csv_Columns_To_Keep[0]: csv_Actual_Row[0],
+		          csv_Columns_To_Keep[1]: csv_Actual_Row[1],
+		          csv_Columns_To_Keep[2]: csv_Actual_Row[2],
+		          csv_Columns_To_Keep[3]: csv_Actual_Row[3],
+		          csv_Columns_To_Keep[4]: csv_Actual_Row[4],
+		          csv_Columns_To_Keep[5]: csv_Actual_Row[5],
+		          csv_Columns_To_Keep[6]: csv_Actual_Row[6],
+		          csv_Columns_To_Keep[7]: csv_Actual_Row[7],
+		          csv_Columns_To_Keep[8]: csv_Actual_Row[8],
+		          csv_Columns_To_Keep[9]: csv_Actual_Row[9],
+		          csv_Columns_To_Keep[10]: csv_Actual_Row[10],
+		          csv_Columns_To_Keep[11]: csv_Actual_Row[11]
+		          })
 
 # <editor-fold desc="Writes the modified data set into all tables of the database">
 # Writes generated data into all tables
 # </editor-fold>
 def write_Data_To_DB():
-        global mongo_dataSet_Row                                # refers to the global variable
-        global mongo_DB_Chicago                                 # refers to the global variable
+		global mongo_DataSet_Row                                # refers to the global variable
+		global mongo_DB_Chicago                                 # refers to the global variable
 
-        # TODO: Expand this shit to write necessary data into all tables !
-        chicago_2014 = mongo_DB_Chicago.chicago_2014
-        chicago_2014.insert_one(mongo_dataSet_Row)
+		# TODO: Expand this shit to write necessary data into all tables !
+		chicago_2014 = mongo_DB_Chicago.chicago_2014
+		chicago_2014.insert_one(mongo_DataSet_Row)
 
 # </editor-fold>
 
 
 
 
-#### !! Reading and converting .CSV files is described below
-
-# <editor-fold desc="Reading and processing .CSV files">
-# Reading und processing of csv-file is done here
-# Every .csv-file will be read and, corresponding to the columns we want to keep, a new .csv-file will be generated
-# Source: http://pandas.pydata.org/pandas-docs/stable/api.html
-# </editor-fold>
 
 
 
 
-#for i in range(starting_Counter, ending_Counter):                                       # Iterates over all .CSV file
-#        print str(i) + ".er Durchgang"
-#        df = pd.read_csv('..\datasets\Crimes_-_' + str(i) +'.csv')
-#        df_new = df[columns_To_keep]
-#        df_new.to_csv('..\datasets\_MODIFIED_Crimes_-_' + str(i) +'.csv', index=False)        # Index=False ignores creation of a counting column
 
 
 
 connect_To_MongoDB()
 get_DB_Instance()
-generate_DataSet_To_Be_Written_To_DB()
-write_Data_To_DB()
+
+read_CSV_Data()
+process_CSV_Data()
