fb4b38384b6be029513f0294544da8c2a3788954
Markus Guder <progamer101101@gmx.de>
2015-12-13 16:59:48 +0100

    Created: Rest DB Call

--- _Projekt/src/server/server.py
+++ _Projekt/src/server/server.py
@@ -1,9 +1,28 @@
+import numpy as np
+from pymongo import MongoClient
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/getData/number-of-tables')
 def hello_world():
-    return 'Hello World!'
+    return mainMethod()
 
 if __name__ == '__main__':
     app.run()
+
+def mainMethod():
+	client = MongoClient()
+	client = MongoClient('localhost', 27017)
+	returnString = "{data:["
+
+	db = client['dataDump']
+	yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
+	array = []
+	mainArray = []
+
+	for year in yearArray:
+	    mainArray.append(db.get_collection(year).count())
+	    returnString = returnString + "[" + year + "," + db.get_collection(year).count() + "]"
+	returnString = returnString + "]"
+
+	return returnString
