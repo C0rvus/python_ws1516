efc8eba074d17e508e885ee2790f426794ed9f03
Markus Guder <progamer101101@gmx.de>
2015-12-16 19:03:11 +0100

    CHANGED: server 1.4

--- _Projekt/src/server/server.py
+++ _Projekt/src/server/server.py
@@ -1,25 +1,24 @@
-import numpy as np
-from pymongo import MongoClient
 from flask import Flask
+from pymongo import MongoClient
 app = Flask(__name__)
 
-@app.route('/getData/number-of-tables')
-def hello_world():
-    return mainMethod()
+@app.route("/getData/number-of-tables", methods=['GET'])
+def hello():
+    return main_Method()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-def mainMethod():
+def main_Method():
 	client = MongoClient()
 	client = MongoClient('localhost', 27017)
-	returnString = "{data:["
+	returnString = "{'data':["
 
 	db = client['dataDump']
 	yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
 
 	for year in yearArray:
-    	returnString = str(returnString) + "[" + str(year) + "," + str(db.get_collection(year).count()) + "]"
-	returnString = returnString + "]}"
+		returnString = str(returnString) + "[" + str(year) + "," + str(db.get_collection(year).count()) + "],"
+	returnString = returnString + "}"
 
 	return returnString
+
+if __name__ == "__main__":
+    app.run()
