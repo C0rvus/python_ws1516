8849dee007645b765deb213632bd2274facbb320
Markus Guder <progamer101101@gmx.de>
2015-12-16 18:28:05 +0100

    Change: 1.3 Server.py

--- _Projekt/src/server/server.py
+++ _Projekt/src/server/server.py
@@ -3,11 +3,14 @@ from pymongo import MongoClient
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/getData/number-of-tables2')
+@app.route('/getData/number-of-tables')
 def hello_world():
-    return mainmethod()
+    return mainMethod()
 
-def mainmethod():
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+def mainMethod():
 	client = MongoClient()
 	client = MongoClient('localhost', 27017)
 	returnString = "{data:["
@@ -16,9 +19,7 @@ def mainmethod():
 	yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
 
 	for year in yearArray:
-		returnString = returnString + "[" + str(year) + "," + str(db.get_collection(year).count()) + "]"
+    	returnString = str(returnString) + "[" + str(year) + "," + str(db.get_collection(year).count()) + "]"
 	returnString = returnString + "]}"
-	return returnString
 
-if __name__ == '__main__':
-    app.run()
+	return returnString
