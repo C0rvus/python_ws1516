fc50cbb3936884ff03c1b3b17fa4e62bda016201
Markus Guder <progamer101101@gmx.de>
2015-12-21 15:03:32 +0100

    Implemented: Highcharts in index.html

--- html/js/uiController/commonCrimeController.js
+++ html/js/uiController/commonCrimeController.js
@@ -1,10 +1,22 @@
 mainController.commonCrimeController = (function () {
-    var that = {}
+    var that = {},
+        data = null
 
         init = function () {
             console.log("commonCrimeController is up");
+            getData();
+            initView();
             return that;
+        },
+
+        getData = function () {
+          console.log("data");
+        },
+
+        initView = function () {
+          commonCrimeView = mainController.commonCrimeView.init(data);
         }
+
 
     that.init = init;
     return that;
--- html/js/uiView/commonCrimeView.js
+++ html/js/uiView/commonCrimeView.js
@@ -1,11 +1,37 @@
 mainController.commonCrimeView = (function () {  var that = {},
 
-      init = function () {
-          console.log("commonCrimeView is up");
+      init = function (data) {
+          console.log("commonCrimeView is up "  + data);
+          initModules();
           return that;
       },
 
       initModules = function () {
+        $('#crimeContainer').highcharts({
+             chart: {
+                 type: 'area',
+                 backgroundColor: 'none'
+             },
+             title: {
+                 text: false
+             },
+             xAxis: {
+                 categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+             },
+             credits: {
+                 enabled: false
+             },
+             series: [{
+                 name: 'John',
+                 data: [5, 3, 4, 7, 2]
+             }, {
+                 name: 'Jane',
+                 data: [2, -2, -3, 2, 1]
+             }, {
+                 name: 'Joe',
+                 data: [3, 4, 4, -2, 5]
+             }]
+         });
       }
 
     that.init = init;
