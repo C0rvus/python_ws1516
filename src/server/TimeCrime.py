70a126053c0b1010d0c84de0b62d73b05def3479
Markus Guder <progamer101101@gmx.de>
2015-12-22 01:33:58 +0100

    Changed fuck of fuck ....

--- src/server/TimeCrime.py
+++ src/server/TimeCrime.py
@@ -4,11 +4,9 @@ from bson.json_util import dumps
 
 class TimeCrime:
 
-    def main_Method(self, year, time):
+    def main_Method(self, year):
         client = MongoClient('localhost', 27017)
         db = client['dataDump']
-        print time
-        print year
         collection = db.get_collection('time')
         collMain = collection.find({'year':str(year)})[0]
         return dumps(collMain)
