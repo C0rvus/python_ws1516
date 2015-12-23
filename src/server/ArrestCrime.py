c1c9417eb612ff16b120cbe50c657f187f4961c2
Markus Guder <progamer101101@gmx.de>
2015-12-23 18:00:57 +0100

    Created: Arrest.py

--- src/server/ArrestCrime.py
+++ src/server/ArrestCrime.py
@@ -6,15 +6,13 @@ class ArrestCrime:
     def main_Method(self):
         client = MongoClient('localhost', 27017)
         db = client['dataDump']
-        yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
         data = {}
         mainArray = []
-        for year in yearArray:
-            subData = {}
-            subData['data'] = [db.get_collection(year).count()]
-            subData['name'] = str(year)
-            mainArray.append(subData)
+        for doc in db.get_collection("arrest").find():
+            if mainArray == []:
+                element = {"name": doc["year"],"y" : doc["arrests"], "sliced": "true", "selected": "true"}
+            else: element = {"name": doc["year"],"y" : doc["arrests"]}
+            mainArray.append(element)
         data['series'] = mainArray
         json_data = json.dumps(data)
-
         return json_data
