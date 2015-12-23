64f75362ab802facbae666a0dfe17db3307d8ab5
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2015-12-23 16:49:30 +0100

    Changed: Made timeCrimeController JSLint conform

--- html/js/uiController/timeCrimeController.js
+++ html/js/uiController/timeCrimeController.js
@@ -1,31 +1,42 @@
+//JSLINT Error-Avoidance here
+/*jslint nomen: true*/ //necessary for underscore-error-avoidance
+
+var $,
+    mainController,
+    jQuery,
+    timeCrimeView;
+
 mainController.timeCrimeController = (function () {
+    'use strict';
+
     var that = {},
         data = null,
         selectedYear = 2001,
         topic = "timeCrime",
         urlRequest = "http://52.29.118.210:5000/" + topic,
 
-        init = function () {
-            _getData();
-            return that;
+        _initView = function (data) {
+            timeCrimeView = mainController.timeCrimeView.init(data);
         },
 
         _getData = function () {
-          $.ajax({
-              url: urlRequest,
-              type: 'GET',
-              crossDomain: true,
-              dataType: 'text',
-              success: function (data) {
-                _initView(jQuery.parseJSON(data));
-              }
-          });
+            $.ajax({
+                url: urlRequest,
+                type: 'GET',
+                crossDomain: true,
+                dataType: 'text',
+                success: function (data) {
+                    _initView(jQuery.parseJSON(data));
+                }
+            });
         },
 
-        _initView = function (data) {
-          timeCrimeView = mainController.timeCrimeView.init(data);
-        }
+
+        init = function () {
+            _getData();
+            return that;
+        };
 
     that.init = init;
     return that;
-})();
+}());
