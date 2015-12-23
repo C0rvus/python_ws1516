86ca729259c6a93dc99ed87e9e0a98a020354246
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2015-12-23 17:34:11 +0100

    Changed: JSLINT Conformity of Views

--- html/js/uiView/commonCrimeView.js
+++ html/js/uiView/commonCrimeView.js
@@ -1,48 +1,53 @@
+var mainController,
+    console,
+    $;
+
 mainController.commonCrimeView = (function () {
-      var that = {},
-      years = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014],
+    'use strict';
+    var that = {},
+        years = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014],
 
-      init = function (data, chartType) {
-          console.log("commonCrimeView is up "  + data);
-          initModules(data, chartType);
-          return that;
-      },
-
-      initModules = function (data, chartType) {
-        $('#crimeContainer').highcharts({
-             chart: {
-                 type: chartType,
-                 backgroundColor: 'none'
-             },
-             //colors:["#00BF77","#0B67BA","#FF9500","#FF4F00"],
-             title: {
-                 text: false
-             },
-             credits: {
-                 enabled: false
-             },
-             xAxis: {
-              categories: years
-             },
-             yAxis: {
-                min: 0,
+        initModules = function (data, chartType) {
+            $('#crimeContainer').highcharts({
+                chart: {
+                    type: chartType,
+                    backgroundColor: 'none'
+                },
+                //colors:["#00BF77","#0B67BA","#FF9500","#FF4F00"],
                 title: {
-                  text: false
-                }
-             },
-             plotOptions: {
-                column: {
-                  stacking: 'percent'
-                }
-             },
-             tooltip: {
-                pointFormat: '<span style="color:<{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.000f}%)<br/>',
-                shared: true
-             },
-             series: data.series
-         });
-      }
+                    text: false
+                },
+                credits: {
+                    enabled: false
+                },
+                xAxis: {
+                    categories: years
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: false
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'percent'
+                    }
+                },
+                tooltip: {
+                    pointFormat: '<span style="color:<{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage:.000f}%)<br/>',
+                    shared: true
+                },
+                series: data.series
+            });
+        },
+        
+        init = function (data, chartType) {
+            console.log("commonCrimeView is up " + data);
+            initModules(data, chartType);
+            return that;
+        };
 
     that.init = init;
     return that;
-})();
+}());
--- html/js/uiView/timeCrimeView.js
+++ html/js/uiView/timeCrimeView.js
@@ -1,59 +1,66 @@
+var mainController,
+    $,
+    console;
+
 mainController.timeCrimeView = (function () {
-      var that = {},
-      years = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014],
+    'use strict';
+    var that = {},
+        years = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014],
 
-      init = function (data) {
-          console.log("timeCrimeView is up "  + data);
-          initModules(data);
-          return that;
-      },
 
-      initModules = function (data) {
-          $('#timeContainer').highcharts({
-              title: {
-                  text: 'Hourly crime rate',
-                  x: -20 //center
-              },
-              subtitle: {
-                  text: 'Cumulated yearly number of crimes at the specific time'
-              },
-              xAxis: {
-                  type: 'datetime',
-                  title: {
-                  	text: 'Time'
-                  },
-                  categories: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
-              },
-              yAxis: {
-                  title: {
-                      text: 'Number of crimes'
-                  },
-                  plotLines: [{
-                      value: 0,
-                      width: 1,
-                      color: '#808080'
-                  }]
-              },
-              plotOptions: {
-                  line: {
-                      dataLabels: {
-                          enabled: true
-                      },
-                      enableMouseTracking: false
-                  }
-              },
-              tooltip: {
-                  valueSuffix: '°C'
-              },
-              legend: {
-                  layout: 'vertical',
-                  align: 'right',
-                  verticalAlign: 'middle',
-                  borderWidth: 0
-              },
-              series: data.series
-          });
-        }
-      that.init = init;
-      return that;
-})();
+
+        initModules = function (data) {
+            $('#timeContainer').highcharts({
+                title: {
+                    text: 'Hourly crime rate',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: 'Cumulated yearly number of crimes at the specific time'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    title: {
+                        text: 'Time'
+                    },
+                    categories: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Number of crimes'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                tooltip: {
+                    valueSuffix: '°C'
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                },
+                series: data.series
+            });
+        },
+        init = function (data) {
+            console.log("timeCrimeView is up " + data);
+            initModules(data);
+            return that;
+        };
+
+    that.init = init;
+    return that;
+}());
