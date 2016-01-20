6c3298100b226becdfed1908268f30a37975d839
Markus Guder <progamer101101@gmx.de>
2016-01-20 22:57:51 +0100

    Fixed: NumberCrime.py

--- src/server/NumberCrime.py
+++ src/server/NumberCrime.py
@@ -6,13 +6,17 @@ import json                                             # necessary for creating
 # __init__() method is not necessary for use - therefore we ignore that warning here
 class NumberCrime:
 
-		def main_Method(self):
-			client = MongoClient()
-			client = MongoClient('localhost', 27017)
-			counter = 0
-			yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
-			db = client['dataDump']
-			for year in yearArray:
-				collectionCount = db.get_collection(year).count()
-				counter = counter + int(collectionCount)
-			return counter
+	def main_Method(self):
+		self.is_Not_Used()
+
+		client = MongoClient('localhost', 27017)
+		counter = 0
+		yearArray = ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014']
+		db = client['dataDump']
+		for year in yearArray:
+			collectionCount = db.get_collection(year).count()
+			counter = counter + int(collectionCount)
+		return counter
+
+	def is_Not_Used(self):                                                  # Necessary to remove 'not static' warning
+			pass
